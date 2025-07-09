b1a8dc35993605b535b5af9d04d16aaebe35d8d3
alilopez <ali.lopez37@gmail.com>
2025-07-08 18:57:28 -0600

    FIX env

--- src/main/java/org/alilopez/Main.java
+++ src/main/java/org/alilopez/Main.java
@@ -3,10 +3,7 @@ package org.alilopez;
 import io.javalin.Javalin;
 import io.javalin.plugin.bundled.CorsPluginConfig;
 import org.alilopez.di.AppModule;
-import org.alilopez.routes.UserRoutes;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
